52f7ec37f883e2a2bc1c38fb98fd8eef31d3455c
Vinicios Gomes <viniciusgomesviana@gmail.com>
2019-08-20 10:43:18 -0300

    Configuração nodemon sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "hello World" });
+  return res.json({ message: "hello Wosrld" });
 });
 
-module.exports = routes;
+export default routes;
