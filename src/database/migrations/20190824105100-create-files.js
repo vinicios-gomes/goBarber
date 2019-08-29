ecc11fcee6a56688eed929c52bc550da122b9eb2
Vinicios Gomes <viniciusgomesviana@gmail.com>
2019-08-29 13:26:45 -0300

    feature/Config and controller File

---
+++ src/database/migrations/20190824105100-create-files.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  }
+};
